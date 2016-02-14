b789dabd3730e8b35b9557c89fbe4517893ebabb
Kevin Jones <kevin.jones@nucleuscentral.com>
2016-02-14 02:34:09 +0000

    lazy push

--- src/sass-builder.js
+++ src/sass-builder.js
@@ -5,7 +5,6 @@ import path from 'path';
 import resolvePath from './resolve-path';
 import escape from './escape-text';
 
-const cssInject = "(function(c){var d=document,a='appendChild',i='styleSheet',s=d.createElement('style');s.type='text/css';d.getElementsByTagName('head')[0][a](s);s[i]?s[i].cssText=c:s[a](d.createTextNode(c));})";
 const isWin = process.platform.match(/^win/);
 
 const loadFile = file => {
